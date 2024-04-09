7bcaef5ae846a2ad6d4b6f42bceb3a5f17f7c335
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-04-08 21:02:31 -0700

    touch ups

--- bokeh-server.py
+++ bokeh-server.py
@@ -12,7 +12,7 @@ from bokeh.palettes import Category20
 from bokeh.plotting import figure, curdoc
 
 ROOT_LAYOUT = 'root'
-DROPDOWN_LAYOUT_NAME = 'dropdown'
+COLUMN_SELECTOR_LAYOUT_NAME = 'column_selector'
 BAR_PLOT_NAME = 'plot1'
 CATEGORY_LIMIT = 19
 
@@ -24,30 +24,66 @@ def load_file(attr, _old_value, new_value):
     file = io.BytesIO(b64decode(new_value))
     metadata = pd.read_csv(file, delimiter='\t')
     print('Successfully loaded metadata.')
+    plot_per_month(metadata)
 
-    columns = metadata.columns.tolist()
+    unique_value_counts = [(col, metadata[col].nunique()) for col in metadata]
 
-    def dropdown_callback(attr, _old_value, column):
+    # Remove columns that have less than 2 and more than 100 unique values
+    unique_value_counts = [(col, n) for col, n in unique_value_counts if 2 <= n <= 100]
+    sorted_counts = sorted(unique_value_counts, key=lambda x: x[1])
+
+    def column_selector_callback(attr, _old_value, column):
         assert attr == 'value'
-        update_plots(metadata, column)
+        plot_stacked_per_month(metadata, column)
 
-    dropdown = Select(title='Select Column', options=[(col, col) for col in columns], name=DROPDOWN_LAYOUT_NAME)
-    dropdown.on_change('value', dropdown_callback)
-    replace_layout(DROPDOWN_LAYOUT_NAME, dropdown)
+    column_selector = Select(
+        name=COLUMN_SELECTOR_LAYOUT_NAME,
+        title='Select column',
+        options=[(col, f"{col} (n={count})") for col, count in sorted_counts],
+    )
+    column_selector.on_change('value', column_selector_callback)
+    replace_layout(COLUMN_SELECTOR_LAYOUT_NAME, column_selector)
 
 
-def sort_months(months):    
+def sort_months(months):
     # sort YYYY-MM strings - a bit hacky
     datetime_objects = [datetime.strptime(month, "%Y-%m") for month in months]
     sorted_datetime_objects = sorted(datetime_objects)
     return [dt.strftime("%Y-%m") for dt in sorted_datetime_objects]
 
-def update_plots(metadata, column):
+
+def plot_per_month(metadata):
     metadata['date_month'] = pd.to_datetime(metadata['date']).dt.strftime("%Y-%m")
     months = sort_months(metadata['date_month'].unique().tolist())
-    
 
-    top_column_values = set(metadata[column].value_counts(sort=True).index[:CATEGORY_LIMIT])
+    count_by_month = metadata['date_month'].value_counts(sort=False).to_dict()
+    counts = [count_by_month[month] for month in months]
+
+    p = figure(name=BAR_PLOT_NAME,
+        x_range=months, height=350,
+        title="distribution over time (months)",
+        toolbar_location="below", tools = "pan,wheel_zoom,box_zoom,reset,hover",
+        tooltips="@x (n=@top)",
+        )
+    p.vbar(x=months, top=counts)
+
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xgrid.grid_line_color = None
+    p.xaxis.major_label_orientation = "vertical"
+    p.axis.minor_tick_line_color = None
+    p.outline_line_color = None
+
+    replace_layout(BAR_PLOT_NAME, p)
+
+
+def plot_stacked_per_month(metadata, column):
+    metadata['date_month'] = pd.to_datetime(metadata['date']).dt.strftime("%Y-%m")
+    months = sort_months(metadata['date_month'].unique().tolist())
+
+    # Convert column to string to ensure categorical
+    metadata[column] = metadata[column].astype(str)
+    top_column_values = set(metadata[column].value_counts(sort=True, ascending=False).index[:CATEGORY_LIMIT])
     metadata['category_adj'] = metadata[column].apply(lambda v: v if v in top_column_values else 'other')
     column_values = metadata['category_adj'].unique().tolist()
 
@@ -59,7 +95,12 @@ def update_plots(metadata, column):
 
     colors = Category20[len(column_values)]
 
-    p = figure(x_range=months, height=350, title="distribution over time (months)", toolbar_location=None, tools="hover", tooltips="$name @time: @$name", name=BAR_PLOT_NAME)
+    p = figure(name=BAR_PLOT_NAME,
+        x_range=months, height=350,
+        title="distribution over time (months)",
+        toolbar_location="below", tools = "pan,wheel_zoom,box_zoom,reset,hover",
+        tooltips="$name @time (n=@$name)",
+        )
     p.vbar_stack(column_values, x='time', source=data, color=colors, legend_label=column_values)
 
     p.y_range.start = 0
@@ -84,7 +125,12 @@ def replace_layout(name, new_layout):
 
 file_input = FileInput(title="Select files:", accept=".tsv")
 file_input.on_change('value', load_file)
-dropdown = Select(title='Select Column (select metadata first)', options=[], name=DROPDOWN_LAYOUT_NAME)
+column_selector = Select(
+    name=COLUMN_SELECTOR_LAYOUT_NAME,
+    title='Select column (select metadata first)',
+    options=[],
+    disabled=True,
+)
 p = figure(name=BAR_PLOT_NAME, height=350)
 
-curdoc().add_root(column(file_input, dropdown, p, name=ROOT_LAYOUT))
+curdoc().add_root(column(file_input, column_selector, p, name=ROOT_LAYOUT))
