e91d073cf1bf94729f904c0ea2666601d86191df
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-04-08 17:08:49 -0700

    stacked bar

--- bokeh-server.py
+++ bokeh-server.py
@@ -7,11 +7,13 @@ import pandas as pd
 
 from bokeh.layouts import column
 from bokeh.models import FileInput, Select
+from bokeh.palettes import Category20
 from bokeh.plotting import figure, curdoc
 
 ROOT_LAYOUT = 'root'
 DROPDOWN_LAYOUT_NAME = 'dropdown'
 BAR_PLOT_NAME = 'plot1'
+CATEGORY_LIMIT = 19
 
 
 def load_file(attr, _old_value, new_value):
@@ -32,12 +34,31 @@ def load_file(attr, _old_value, new_value):
     def update_plots(attr, _old_value, column):
         assert attr == 'value'
 
-        value_counts = metadata[column].value_counts()
-        columns = list(value_counts.index)
-        counts = list(value_counts)
+        metadata['date_month'] = pd.to_datetime(metadata['date']).dt.strftime("%Y-%m")
+        months = metadata['date_month'].unique().tolist()
 
-        p = figure(x_range=columns, height=350, title="Column summary", name=BAR_PLOT_NAME)
-        p.vbar(x=columns, top=counts, width=0.9)
+        top_column_values = set(metadata[column].value_counts(sort=True).index[:CATEGORY_LIMIT])
+        metadata['category_adj'] = metadata[column].apply(lambda v: v if v in top_column_values else 'other')
+        column_values = metadata['category_adj'].unique().tolist()
+
+        data = {
+            'time': months
+        }
+        for category in column_values:
+            data[category] = [len(metadata.query(f"{'category_adj'} == {category!r} & date_month == {month!r}")) for month in months]
+
+        colors = Category20[len(column_values)]
+
+        p = figure(x_range=months, height=350, title="distribution over time", toolbar_location=None, tools="hover", tooltips="$name @time: @$name", name=BAR_PLOT_NAME)
+        p.vbar_stack(column_values, x='time', source=data, color=colors, legend_label=column_values)
+
+        p.y_range.start = 0
+        p.x_range.range_padding = 0.1
+        p.xgrid.grid_line_color = None
+        p.axis.minor_tick_line_color = None
+        p.outline_line_color = None
+        p.legend.location = "top_left"
+        p.legend.orientation = "horizontal"
 
         replace_layout(BAR_PLOT_NAME, p)
 
@@ -47,12 +68,12 @@ def load_file(attr, _old_value, new_value):
     replace_layout(DROPDOWN_LAYOUT_NAME, dropdown)
 
 
-def replace_layout(name, new_plot):
+def replace_layout(name, new_layout):
     sublayouts = curdoc().get_model_by_name(ROOT_LAYOUT).children
     old_layout = curdoc().get_model_by_name(name)
     for i, layout in enumerate(sublayouts):
         if layout == old_layout:
-            sublayouts[i] = new_plot
+            sublayouts[i] = new_layout
 
 
 file_input = FileInput(title="Select files:", accept=".tsv")
