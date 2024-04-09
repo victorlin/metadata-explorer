807b55519d26e8907b9e5cc829ec0fe39a57d989
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-04-08 17:29:02 -0700

    fix date sorting

--- bokeh-server.py
+++ bokeh-server.py
@@ -2,6 +2,7 @@
 #   bokeh serve --show bokeh-server.py
 
 from base64 import b64decode
+from datetime import datetime
 import io
 import pandas as pd
 
@@ -17,7 +18,6 @@ CATEGORY_LIMIT = 19
 
 
 def load_file(attr, _old_value, new_value):
-    global columns
     assert attr == 'value'
 
     print('Loading metadata...')
@@ -27,45 +27,50 @@ def load_file(attr, _old_value, new_value):
 
     columns = metadata.columns.tolist()
 
-    # This isn't great - a callback within a callback. Doing this because I
-    # can't figure out how to dynamically update the same layout. Replacing it
-    # entirely as a workaround, which necessitates nested callback to retain the
-    # user input sequence.
-    def update_plots(attr, _old_value, column):
+    def dropdown_callback(attr, _old_value, column):
         assert attr == 'value'
-
-        metadata['date_month'] = pd.to_datetime(metadata['date']).dt.strftime("%Y-%m")
-        months = metadata['date_month'].unique().tolist()
-
-        top_column_values = set(metadata[column].value_counts(sort=True).index[:CATEGORY_LIMIT])
-        metadata['category_adj'] = metadata[column].apply(lambda v: v if v in top_column_values else 'other')
-        column_values = metadata['category_adj'].unique().tolist()
-
-        data = {
-            'time': months
-        }
-        for category in column_values:
-            data[category] = [len(metadata.query(f"{'category_adj'} == {category!r} & date_month == {month!r}")) for month in months]
-
-        colors = Category20[len(column_values)]
-
-        p = figure(x_range=months, height=350, title="distribution over time", toolbar_location=None, tools="hover", tooltips="$name @time: @$name", name=BAR_PLOT_NAME)
-        p.vbar_stack(column_values, x='time', source=data, color=colors, legend_label=column_values)
-
-        p.y_range.start = 0
-        p.x_range.range_padding = 0.1
-        p.xgrid.grid_line_color = None
-        p.axis.minor_tick_line_color = None
-        p.outline_line_color = None
-        p.legend.location = "top_left"
-        p.legend.orientation = "horizontal"
-
-        replace_layout(BAR_PLOT_NAME, p)
-
+        update_plots(metadata, column)
 
     dropdown = Select(title='Select Column', options=[(col, col) for col in columns], name=DROPDOWN_LAYOUT_NAME)
-    dropdown.on_change('value', update_plots)
+    dropdown.on_change('value', dropdown_callback)
     replace_layout(DROPDOWN_LAYOUT_NAME, dropdown)
+
+
+def sort_months(months):    
+    # sort YYYY-MM strings - a bit hacky
+    datetime_objects = [datetime.strptime(month, "%Y-%m") for month in months]
+    sorted_datetime_objects = sorted(datetime_objects)
+    return [dt.strftime("%Y-%m") for dt in sorted_datetime_objects]
+
+def update_plots(metadata, column):
+    metadata['date_month'] = pd.to_datetime(metadata['date']).dt.strftime("%Y-%m")
+    months = sort_months(metadata['date_month'].unique().tolist())
+    
+
+    top_column_values = set(metadata[column].value_counts(sort=True).index[:CATEGORY_LIMIT])
+    metadata['category_adj'] = metadata[column].apply(lambda v: v if v in top_column_values else 'other')
+    column_values = metadata['category_adj'].unique().tolist()
+
+    data = {
+        'time': months
+    }
+    for category in column_values:
+        data[category] = [len(metadata.query(f"{'category_adj'} == {category!r} & date_month == {month!r}")) for month in months]
+
+    colors = Category20[len(column_values)]
+
+    p = figure(x_range=months, height=350, title="distribution over time (months)", toolbar_location=None, tools="hover", tooltips="$name @time: @$name", name=BAR_PLOT_NAME)
+    p.vbar_stack(column_values, x='time', source=data, color=colors, legend_label=column_values)
+
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xgrid.grid_line_color = None
+    p.axis.minor_tick_line_color = None
+    p.outline_line_color = None
+    p.legend.location = "top_left"
+    p.legend.orientation = "horizontal"
+
+    replace_layout(BAR_PLOT_NAME, p)
 
 
 def replace_layout(name, new_layout):
